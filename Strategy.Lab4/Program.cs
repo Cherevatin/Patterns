15c1d57a5d15f20d64af27b669b4b2a38328d38a
Cherevatin <cherevatin3356@gmail.com>
2023-05-03 12:24:44 +0300

    added lab4

---
+++ Strategy.Lab4/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+var deliveryApp = new DeliveryApp(new Pickup());
+Console.WriteLine(deliveryApp.Delivery.GetType() + " " + deliveryApp.Delivery.CalculatePrice());
+
+deliveryApp.SetDelivery(new InternalDeliveryService());
+Console.WriteLine(deliveryApp.Delivery.GetType() + " " + deliveryApp.Delivery.CalculatePrice());
+
+deliveryApp.SetDelivery(new ExternalDeliveryService());
+Console.WriteLine(deliveryApp.Delivery.GetType() + " " + deliveryApp.Delivery.CalculatePrice());
+
+public class DeliveryApp
+{
+    public IDelivery Delivery { get; set; }
+
+    public DeliveryApp(IDelivery delivery)
+    {
+        Delivery = delivery;
+    }
+
+    public void SetDelivery(IDelivery delivery)
+    {
+        Delivery = delivery;
+    }
+}
+
+public interface IDelivery
+{
+    public int CalculatePrice();
+}
+
+public class Pickup : IDelivery
+{
+    public int CalculatePrice()
+    {
+        return 0;
+    }
+}
+
+public class InternalDeliveryService : IDelivery
+{
+    public int CalculatePrice()
+    {
+        return 100;
+    }
+}
+
+public class ExternalDeliveryService : IDelivery
+{
+    public int CalculatePrice()
+    {
+        return 150;
+    }
+}
