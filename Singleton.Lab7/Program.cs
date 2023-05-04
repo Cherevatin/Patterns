f186fcc0b650735e8d73dc60adcc07b11cc330ae
Cherevatin <cherevatin3356@gmail.com>
2023-05-04 10:14:22 +0300

    added lab7

---
+++ Singleton.Lab7/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+var mongodb = OS.GetMongoDBConnection();
+var postgres = OS.GetPostgreSQLConnection();
+
+if (mongodb == OS.GetMongoDBConnection())
+{
+    Console.WriteLine("Same instances.");
+}
+
+if (postgres == OS.GetPostgreSQLConnection())
+{
+    Console.WriteLine("Same instances.");
+}
+
+public class PostgreSQL
+{
+    public void Connect()
+    {
+        Console.WriteLine("Trying to connect PostgreSQL");
+    }
+}
+
+public class MongoDB
+{
+    public void Connect()
+    {
+        Console.WriteLine("Trying to connect MongoDB");
+    }
+}
+
+public class OS
+{
+    private static MongoDB MongoDB;
+    private static PostgreSQL PostgreSQL;
+
+    public OS()
+    { }
+
+    public static MongoDB GetMongoDBConnection()
+    {
+        if (MongoDB == null)
+        {
+            MongoDB = new MongoDB();
+            MongoDB.Connect();
+        }
+        return MongoDB;
+    }
+
+    public static PostgreSQL GetPostgreSQLConnection()
+    {
+        if (PostgreSQL == null)
+        {
+            PostgreSQL = new PostgreSQL();
+            PostgreSQL.Connect();
+        }
+        return PostgreSQL;
+    }
+}
